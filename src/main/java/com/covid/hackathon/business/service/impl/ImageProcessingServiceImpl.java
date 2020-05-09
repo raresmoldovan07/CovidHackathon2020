59b14722bed0f17e9fe9ac3faaea5b60e136aa37
raresmoldovan07 <raresmoldovan00@gmail.com>
2020-05-09 17:52:51 +0300

    Methods refactoring

--- src/main/java/com/covid/hackathon/business/service/impl/ImageProcessingServiceImpl.java
+++ src/main/java/com/covid/hackathon/business/service/impl/ImageProcessingServiceImpl.java
@@ -4,7 +4,6 @@ import com.covid.hackathon.business.service.ImageProcessingService;
 import com.covid.hackathon.business.util.Converter;
 import com.covid.hackathon.model.FormattedImage;
 import com.covid.hackathon.model.Image;
-import com.fasterxml.jackson.databind.JsonNode;
 import net.minidev.json.JSONObject;
 import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
@@ -28,6 +27,8 @@ public class ImageProcessingServiceImpl implements ImageProcessingService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ImageProcessingServiceImpl.class);
 
     private static final String IMAGE_EXTENSION = "png";
+    private static final String IMAGE_PATH = "imagePath";
+    private static final String PYTHON_SERVER_URI = "http://localhost:8082/processImage";
 
     private Base64 base64;
     private RestTemplate restTemplate;
@@ -40,30 +41,51 @@ public class ImageProcessingServiceImpl implements ImageProcessingService {
         this.restTemplate = restTemplate;
     }
 
-    private String requestImageFormat(String imagePath){
-        String uri = "http://localhost:8082/processImage";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        JSONObject path = new JSONObject();
-        path.put("imagePath", imagePath);
-        HttpEntity<String> request =new HttpEntity<String>(path.toString(), headers);
-        FormattedImage formattedImage = restTemplate.postForObject(uri, request, FormattedImage.class);
-        return formattedImage.getImagePath();
-    }
-
     @Override
     public Image processImage(Image image) throws IOException {
         LOGGER.info("Processing new image");
+        String imagePath = saveImage(image);
+        String processedImageFileName = requestImageProcessing(imagePath);
+        return loadProcessedImage(processedImageFileName);
+    }
+
+    private String saveImage(Image image) throws IOException {
         String imageFileName = String.format("%s.%s", UUID.randomUUID(), IMAGE_EXTENSION);
+        LOGGER.info("Saving new image in {}", imageFileName);
         File outputfile = new File(imageFileName);
         BufferedImage bufferedImage = converter.convertBase64ToBufferedImage(image.getBase64Code());
-        ImageIO.write(bufferedImage, IMAGE_EXTENSION, outputfile);
+        try {
+            ImageIO.write(bufferedImage, IMAGE_EXTENSION, outputfile);
+            return imageFileName;
+        } catch (IOException e) {
+            LOGGER.error("Failed to save image", e);
+            throw e;
+        }
+    }
 
-        String modifiedImagePath = requestImageFormat(imageFileName);
-
-        bufferedImage = ImageIO.read(new File(modifiedImagePath));
+    private Image loadProcessedImage(String imageFileName) throws IOException {
+        LOGGER.info("Loading processed image from {}", imageFileName);
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = ImageIO.read(new File(imageFileName));
+        } catch (IOException e) {
+            LOGGER.error("Failed to load processed image", e);
+            throw e;
+        }
         String base64String = converter.convertBufferedImageToBase64(bufferedImage);
-        image.setBase64Code(base64String);
-        return image;
+        return new Image(base64String);
+    }
+
+    private String requestImageProcessing(String imagePath){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        JSONObject path = new JSONObject();
+        path.put(IMAGE_PATH, imagePath);
+        HttpEntity<String> request =new HttpEntity<>(path.toString(), headers);
+        FormattedImage formattedImage = restTemplate.postForObject(PYTHON_SERVER_URI, request, FormattedImage.class);
+        if (formattedImage == null) {
+            throw new RuntimeException("Failed to receive formatted image");
+        }
+        return formattedImage.getImagePath();
     }
 }
